b7165ee25f95a629d24a21f921658636658e7843
KyuoFoxHuyu <88403766@qq.com>
2022-02-09 12:52:54 +0800

    build-tc.sh: defines O3
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- build-tc.sh
+++ build-tc.sh
@@ -22,7 +22,7 @@ msg "Building LLVM..."
 	--install-folder "installTmp" \
 	--clang-vendor "Kyuofox-$(date +%Y%m%d)" \
 	--additional-build-arguments "CLANG_REPOSITORY_STRING=GitHub.com/KyuoFoxHuyu" \
-	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc)"
+	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3"
 
 # Build binutils
 msg "Building binutils..."
